d1d32062b99d1fef7f73e1e8b230c5ac23e4d940
YuJeeun <dbwpdms1122@naver.com>
2024-06-21 19:14:43 +0900

    [level 0] Title: 수 조작하기 1, Time: 4.39 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181926. 수 조작하기 1/수 조작하기 1.py
@@ -0,0 +1,16 @@
+def solution(n, control):
+    for i in range(len(control)):
+        if control[i]=='w':
+            n+=1
+
+        if control[i]=='s':
+            n-=1
+     
+        if control[i]=='d':
+            n+=10
+         
+        if control[i]=='a':
+            n-=10
+    
+    answer = n
+    return answer

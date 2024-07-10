e160c53afa82552712b949c3a8ce929eb68488f3
YuJeeun <dbwpdms1122@naver.com>
2024-07-10 18:27:49 +0900

    [level 0] Title: A 강조하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.py
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.py
@@ -1,15 +1,14 @@
 def solution(myString):
     answer = ''
-    temp1=''
-    temp2=''
-    for i in range (len(myString)):
-        if 'a' in myString[i]:
-            answer+=myString[i].replace(myString[i],myString[i].upper())
-        elif myString[i].isupper():
-            if myString[i]!='A':
-                answer+=myString[i].replace(myString[i],myString[i].lower())
-            else:
-                answer+=myString[i]
-        else:
-            answer+=myString[i]
+    #for i in range (len(myString)):
+     #   if 'a' in myString[i]:
+    #        answer+=myString[i].replace(myString[i],myString[i].upper())
+   #     elif myString[i].isupper():
+  #          if myString[i]!='A':
+#                answer+=myString[i].replace(myString[i],myString[i].lower())
+#            else:
+#                answer+=myString[i]
+#        else:
+ #           answer+=myString[i]
+    answer=myString.lower().replace('a','A')
     return answer
